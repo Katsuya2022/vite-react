9d964754ed9fe0f7a97883040371733acf427cdd
Katsuya2022 <98100842+Katsuya2022@users.noreply.github.com>
2025-06-13 12:28:28 +0900

    win_サイドメニューのデータファイル作成

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './SideMenu.css'
+import SideMenuDatas from './SideMenuDatas';
 
 const SideMenu = () => {
+  const list = SideMenuDatas.map((data, index) => {
+    return <a href={data.link}>{`${index}: ${data.text}`}</a>
+  })
   return (
     <div className='side-menu'>
       side-menu
+      <div>
+        {list}
+      </div>
     </div>
   )
 }
---
+++ src/components/SideMenu/SideMenuDatas.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import HomeIcon from '@mui/icons-material/Home';
+
+const SideMenuDatas = [
+  {
+    text: 'Home',
+    icon: <HomeIcon />,
+    link: '/'
+  },
+  {
+    text: 'Page1',
+    icon: '',
+    link: '/page1'
+  },
+  {
+    text: 'Page2',
+    icon: '',
+    link: '/page2'
+  },
+];
+
+export default SideMenuDatas;
