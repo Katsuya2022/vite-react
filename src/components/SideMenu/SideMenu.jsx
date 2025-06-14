3df2e18cac433233ce81b264dedabe9126a3f3a5
Katsuya2022 <98100842+Katsuya2022@users.noreply.github.com>
2025-06-14 12:59:26 +0900

    サイドメニューCSS修正中、変なpaddingが右についている、、

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import './SideMenu.css'
 import SideMenuDatas from './SideMenuDatas';
+import { useNavigate } from 'react-router-dom';
 
 const SideMenu = () => {
-  const list = SideMenuDatas.map((data, index) => (
-    <div key={index}>
-      <a href={data.link}>
-        {data.icon}
-        {data.text}
-      </a>
-    </div>
-  ))
+  const navigate = useNavigate();
+  const changePage = (page) => {
+    navigate(page);
+  }
+
   return (
     <div className='side-menu'>
-      side-menu
-      <div>
-        {list}
-      </div>
+      <ul className='side-menu-list'>
+        {SideMenuDatas.map((data, index) => {
+          return (
+            <li key={index} className='row' onClick={() => changePage(data.link)}>
+              <div id='icon'>{data.icon}</div>
+              <div id='text'>{data.text}</div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   )
 }
