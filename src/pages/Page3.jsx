4546e16b8f6a39a9d2d26763f19b7c4ef84b1aa2
Katsuya2022 <98100842+Katsuya2022@users.noreply.github.com>
2025-06-17 09:48:28 +0900

    supabaseへの登録処理を実装

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -4,22 +4,51 @@ import { supabase } from '../lib/supabase';
 
 const Page3 = () => {
   const [response, setResponse] = useState(null);
+  const [todo, setTodo] = useState('');
+  const [errorText, setErrorText] = useState('');
 
+  /**
+   * 初期表示時のTodo情報を取得する処理を呼び出す
+   */
   useEffect(() => {
-    const fetchData = async () => {
-      const { data, error } = await supabase.from('todos').select();
-      if (error) {
-        console.error(error);
-      } else {
-        setResponse(data);
-      }
-    };
     fetchData();
   }, []);
+
+  /**
+   * Todoを取得する
+   */
+  const fetchData = async () => {
+    const { data, error } = await supabase.from('todos').select();
+    if (error) {
+      console.error(error);
+    } else {
+      setResponse(data);
+    }
+  };
+
+  /**
+   * Todoを登録する
+   */
+  const registTodo = async () => {
+    if (todo === '') {
+      setErrorText('Todoを入力してください。');
+      return;
+    }
+    const { error } = await supabase.from('todos').insert({title: todo});
+    if (error) {
+      setErrorText(error.message);
+    } else {
+      setErrorText('');
+      fetchData();
+    }
+  }
 
   return (
     <div className='page3'>
       <h1>Page3</h1>
+      <input type="text" name="todo" id="todo" value={todo} onChange={(e) => {setTodo(e.target.value)}}/>
+      <p className='text-danger'>{errorText}</p>
+      <button className='btn btn-primary' onClick={() => registTodo()}>登録</button>
       <pre>{response ? JSON.stringify(response, null, 2) : 'Loading...'}</pre>
     </div>
   )
