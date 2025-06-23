6ad53e1ecbae8a4eb8afbb6fa49117d1adf20dcf
Katsuya2022 <98100842+Katsuya2022@users.noreply.github.com>
2025-06-23 09:11:22 +0900

    開発用DB取得データ表示画面の簡易実装

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+import { supabase } from '../lib/supabase';
+
 const Page2 = () => {
+    const [response, setResponse] = useState(null);
+    const [tableName, setTableName] = useState('');
+
+    /**
+     * Todoを取得する
+     */
+    const fetchData = async () => {
+      const { data, error } = await supabase.from(tableName).select().order('id', {ascending: true});
+      if (error) {
+        console.error(error);
+        setResponse(null);
+      } else {
+        setResponse(data);
+      }
+    };
+
   return (
     <div className='page2'>
-      <h1>page2</h1>
+      <h1>データ一覧</h1>
+            <div className='controll-area'>
+        <input
+          id="table-name"
+          className='table-name-input'
+          type="text"
+          placeholder='テーブル名を入力してください。'
+          value={tableName}
+          onChange={(e) => setTableName(e.target.value)}
+        />
+        <button className='btn btn-primary' onClick={fetchData}>検索</button>
+      </div>
+      <pre>
+        {JSON.stringify(response, null, 2)}
+      </pre>
     </div>
   )
 
