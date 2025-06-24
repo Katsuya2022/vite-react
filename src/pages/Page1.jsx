9173012d124cad8afe005adf6247f9e47357e086
Katsuya2022 <98100842+Katsuya2022@users.noreply.github.com>
2025-06-24 09:44:39 +0900

    win_担当工程チェックボックス追加

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -13,69 +13,86 @@ const Page1 = () => {
             ログインユーザーの情報とは異なる情報を表示したいときはこちらに入力してください。
           </p>
           <form>
+            {/* 表示名（漢字） */}
             <div className="row mb-3">
               <label htmlFor="name" className="col-sm-3 col-form-label">表示名（漢字）</label>
               <div className="col-sm-9">
                 <input type="text" className="form-control" id="name" />
               </div>
             </div>
+
+            {/* 表示名（カナ） */}
             <div className="row mb-3">
               <label htmlFor="name-kana" className="col-sm-3 col-form-label">表示名（カナ）</label>
               <div className="col-sm-9">
                 <input type="text" className="form-control" id="name-kana" />
               </div>
             </div>
+
+            {/* 年齢 */}
             <div className="row mb-3">
               <label htmlFor="age" className="col-sm-3 col-form-label">年齢</label>
               <div className="col-sm-9">
                 <input type="text" inputMode="numeric" pattern="\d*" maxLength={3} className="form-control" id="age" />
               </div>
             </div>
+            
+            {/* Eメール */}
             <div className="row mb-3">
               <label htmlFor="email" className="col-sm-3 col-form-label">Eメール</label>
               <div className="col-sm-9">
                 <input type="email" className="form-control" id="email" />
               </div>
             </div>
-            <div className="row mb-3">
-              <label htmlFor="inputPassword3" className="col-sm-3 col-form-label">Password</label>
-              <div className="col-sm-9">
-                <input type="password" className="form-control" id="inputPassword3" />
-              </div>
-            </div>
+
+            {/* 担当工程 */}
             <fieldset className="row mb-3">
-              <legend className="col-form-label col-sm-3 pt-0">Radios</legend>
+              <legend className="col-form-label col-sm-3 pt-0">担当工程</legend>
               <div className="col-sm-9">
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" />
-                  <label className="form-check-label" htmlFor="gridRadios1">
-                    First radio
+                  <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                  <label className="form-check-label" htmlFor="gridCheck1">
+                    要件定義
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2" />
-                  <label className="form-check-label" htmlFor="gridRadios2">
-                    Second radio
+                  <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                  <label className="form-check-label" htmlFor="gridCheck1">
+                    基本設計
                   </label>
                 </div>
-                <div className="form-check disabled">
-                  <input className="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled />
-                  <label className="form-check-label" htmlFor="gridRadios3">
-                    Third disabled radio
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                  <label className="form-check-label" htmlFor="gridCheck1">
+                    詳細設計
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                  <label className="form-check-label" htmlFor="gridCheck1">
+                    実装・単体
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                  <label className="form-check-label" htmlFor="gridCheck1">
+                    結合テスト
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                  <label className="form-check-label" htmlFor="gridCheck1">
+                    総合テスト
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                  <label className="form-check-label" htmlFor="gridCheck1">
+                    保守・運用
                   </label>
                 </div>
               </div>
             </fieldset>
-            <div className="row mb-3">
-              <div className="col-sm-9 offset-sm-3">
-                <div className="form-check">
-                  <input className="form-check-input" type="checkbox" id="gridCheck1" />
-                  <label className="form-check-label" htmlFor="gridCheck1">
-                    Example checkbox
-                  </label>
-                </div>
-              </div>
-            </div>
             <button type="submit" className="btn btn-primary">Sign in</button>
           </form>
         </div>
