8867194461ff32400e4701635c3d10a13fd5c574
Katsuya2022 <98100842+Katsuya2022@users.noreply.github.com>
2025-06-26 00:54:05 +0900

    mac_案件をコンポーネント化、map関数で繰り返し表示できるよう実装

---
+++ src/components/SkillSheet/ProjectForm.jsx
@@ -0,0 +1,122 @@
+import { useState } from 'react'
+
+const ProjectForm = ({title, visible}) => {
+  const [displayInputArea, setDisplayInputArea] = useState(false);
+  const toggleDisplay = () => {
+    setDisplayInputArea(!displayInputArea);
+  }
+  const toggleText = visible ? '閉じる' : '入力する';
+  
+  return (
+    <div id="input-area" className="p-4 rounded border border-primary mb-4">
+      <div className='d-flex'>
+        <h2>{title}</h2>
+        <button className='btn btn-link' onClick={toggleDisplay}>{toggleText}</button>
+      </div>
+      {
+        displayInputArea &&
+        <form>
+          {/* 案件名 */}
+          <div className="row mb-3">
+            <label htmlFor="email" className="col-sm-3 col-form-label">案件名</label>
+            <div className="col-sm-9">
+              <input type="email" className="form-control" id="email" />
+            </div>
+          </div>
+          
+          {/* 開始日 */}
+          <div className="row mb-3">
+            <label htmlFor="email" className="col-sm-3 col-form-label">開始日</label>
+            <div className="col-sm-9">
+              <input type="email" className="form-control" id="email" />
+            </div>
+          </div>
+          
+          {/* 終了日 */}
+          <div className="row mb-3">
+            <label htmlFor="email" className="col-sm-3 col-form-label">終了日</label>
+            <div className="col-sm-9">
+              <input type="email" className="form-control" id="email" />
+            </div>
+          </div>
+          
+          {/* 案件内容 */}
+          <div className="row mb-3">
+            <label htmlFor="email" className="col-sm-3 col-form-label">案件内容</label>
+            <div className="col-sm-9">
+              <input type="email" className="form-control" id="email" />
+            </div>
+          </div>
+          
+          {/* 言語 */}
+          <div className="row mb-3">
+            <label htmlFor="email" className="col-sm-3 col-form-label">言語</label>
+            <div className="col-sm-9">
+              <input type="email" className="form-control" id="email" />
+            </div>
+          </div>
+          
+          {/* DB・FW・ツール等 */}
+          <div className="row mb-3">
+            <label htmlFor="email" className="col-sm-3 col-form-label">DB・FW・ツール等</label>
+            <div className="col-sm-9">
+              <input type="email" className="form-control" id="email" />
+            </div>
+          </div>
+
+          {/* 担当工程 */}
+          <fieldset className="row mb-3">
+            <legend className="col-form-label col-sm-3 pt-0">担当工程</legend>
+            <div className="col-sm-9">
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                <label className="form-check-label" htmlFor="gridCheck1">
+                  要件定義
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                <label className="form-check-label" htmlFor="gridCheck1">
+                  基本設計
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                <label className="form-check-label" htmlFor="gridCheck1">
+                  詳細設計
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                <label className="form-check-label" htmlFor="gridCheck1">
+                  実装・単体
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                <label className="form-check-label" htmlFor="gridCheck1">
+                  結合テスト
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                <label className="form-check-label" htmlFor="gridCheck1">
+                  総合テスト
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                <label className="form-check-label" htmlFor="gridCheck1">
+                  保守・運用
+                </label>
+              </div>
+            </div>
+          </fieldset>
+          <button type="submit" className="btn btn-primary">保存する</button>
+        </form>
+      }
+    </div>
+  )
+}
+
+export default ProjectForm
--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,24 +1,30 @@
 import { useState } from 'react'
+import ProjectForm from '../components/SkillSheet/ProjectForm';
 import './Page1.css'
 
 const Page1 = () => {
   const [displayKihon, setDisplayKihon] = useState(true);
-  const toggleDisplay = (event) => {
-    event.preventDefault();
+  const [projects, setProjects] = useState([
+    {id: 'project1', title: '案件1', visible: true},
+    {id: 'project2', title: '案件2', visible: true},
+  ]);
+  const toggleDisplay = () => {
     setDisplayKihon(!displayKihon);
   }
 
+  const kihonToggleText = displayKihon ? '閉じる' : '入力する';
+
   return (
     <div className='page1'>
-      <h1>Skill-Sheet</h1>
+      <h1>Skill Sheet</h1>
       <div className="d-flex gap-4 p-4">
         {/* 左エリア */}
         <div className='d-flex flex-column left-area'>
           {/* 基本情報 */}
-          <div id="input-area" className="p-4 rounded border border-primary  mb-4">
+          <div id="input-area" className="p-4 rounded border border-primary  mb-4 kihon-area">
             <div className='d-flex'>
               <h2>基本情報</h2>
-              <a onClick={toggleDisplay}>表示</a>
+              <button className='btn btn-link' onClick={toggleDisplay}>{kihonToggleText}</button>
             </div>
             <p>
               基本的にはログインユーザーの情報が表示されます。<br />
@@ -26,7 +32,7 @@ const Page1 = () => {
             </p>
             {
               displayKihon && 
-              <div className='input-area-kihon'>
+              <div>
                 <form>
                   {/* 表示名（漢字） */}
                   <div className="row mb-3">
@@ -65,109 +71,12 @@ const Page1 = () => {
             }
           </div>
 
-          {/* 案件１ */}
-          <div id="input-area" className="p-4 rounded border border-primary">
-            <h2>案件１</h2>
-            <form>
-              {/* 案件名 */}
-              <div className="row mb-3">
-                <label htmlFor="email" className="col-sm-3 col-form-label">案件名</label>
-                <div className="col-sm-9">
-                  <input type="email" className="form-control" id="email" />
-                </div>
-              </div>
-              
-              {/* 開始日 */}
-              <div className="row mb-3">
-                <label htmlFor="email" className="col-sm-3 col-form-label">開始日</label>
-                <div className="col-sm-9">
-                  <input type="email" className="form-control" id="email" />
-                </div>
-              </div>
-              
-              {/* 終了日 */}
-              <div className="row mb-3">
-                <label htmlFor="email" className="col-sm-3 col-form-label">終了日</label>
-                <div className="col-sm-9">
-                  <input type="email" className="form-control" id="email" />
-                </div>
-              </div>
-              
-              {/* 案件内容 */}
-              <div className="row mb-3">
-                <label htmlFor="email" className="col-sm-3 col-form-label">案件内容</label>
-                <div className="col-sm-9">
-                  <input type="email" className="form-control" id="email" />
-                </div>
-              </div>
-              
-              {/* 言語 */}
-              <div className="row mb-3">
-                <label htmlFor="email" className="col-sm-3 col-form-label">言語</label>
-                <div className="col-sm-9">
-                  <input type="email" className="form-control" id="email" />
-                </div>
-              </div>
-              
-              {/* DB・FW・ツール等 */}
-              <div className="row mb-3">
-                <label htmlFor="email" className="col-sm-3 col-form-label">DB・FW・ツール等</label>
-                <div className="col-sm-9">
-                  <input type="email" className="form-control" id="email" />
-                </div>
-              </div>
-
-              {/* 担当工程 */}
-              <fieldset className="row mb-3">
-                <legend className="col-form-label col-sm-3 pt-0">担当工程</legend>
-                <div className="col-sm-9">
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
-                    <label className="form-check-label" htmlFor="gridCheck1">
-                      要件定義
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
-                    <label className="form-check-label" htmlFor="gridCheck1">
-                      基本設計
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
-                    <label className="form-check-label" htmlFor="gridCheck1">
-                      詳細設計
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
-                    <label className="form-check-label" htmlFor="gridCheck1">
-                      実装・単体
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
-                    <label className="form-check-label" htmlFor="gridCheck1">
-                      結合テスト
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
-                    <label className="form-check-label" htmlFor="gridCheck1">
-                      総合テスト
-                    </label>
-                  </div>
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
-                    <label className="form-check-label" htmlFor="gridCheck1">
-                      保守・運用
-                    </label>
-                  </div>
-                </div>
-              </fieldset>
-              <button type="submit" className="btn btn-primary">保存する</button>
-            </form>
-          </div>
+          {/* 案件 */}
+          {
+            projects.map((project, index) => (
+              <ProjectForm key={index} title={project.title} visible={project.visible} />
+            ))
+          }
         </div>
 
         {/* 右エリア */}
