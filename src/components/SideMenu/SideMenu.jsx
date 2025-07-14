2eb4f7292b18b97f1fee6c626f16fea3da2e52eb
Katsuya2022 <98100842+Katsuya2022@users.noreply.github.com>
2025-07-15 00:13:38 +0900

    mac_ログインの有無に合わせてサイドメニューの内容を変更するよう修正

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './SideMenu.css'
 import SideMenuDatas from './SideMenuDatas';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 
 const SideMenu = () => {
-  const user = useAuth();
+  const { user } = useAuth();
   const navigate = useNavigate();
   const changePage = (page) => {
     navigate(page);
@@ -14,23 +14,21 @@ const SideMenu = () => {
   return (
     <div className='side-menu'>
       <ul className='side-menu-list'>
-        {SideMenuDatas.map((data, index) => {
+        {
           // ログインしていない場合はホーム以外のボタンは表示しない
-          if (!user && data.text !== 'Home') {
-            return null
-          };
-          return (
-            <li 
-              id={window.location.pathname === data.link ? 'active' : ''} 
-              className='menu-item'
-              key={index}
-              onClick={() => changePage(data.link)}
-            >
-              <div id='icon'>{data.icon}</div>
-              <div id='text'>{data.text}</div>
-            </li>
-          );
-        })}
+          (user ? SideMenuDatas : SideMenuDatas.filter(data => data.text === 'Home'))
+            .map((data, index) => (
+              <li
+                id={window.location.pathname === data.link ? 'active' : ''}
+                className='menu-item'
+                key={index}
+                onClick={() => changePage(data.link)}
+              >
+                <div id='icon'>{data.icon}</div>
+                <div id='text'>{data.text}</div>
+              </li>
+            ))
+        }
       </ul>
     </div>
   )
