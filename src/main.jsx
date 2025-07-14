405bf9c1089daeaa10ba4680450c224ad116fbfc
Katsuya2022 <98100842+Katsuya2022@users.noreply.github.com>
2025-07-14 16:03:43 +0900

    win_Providersコンポーネントを作成し、共通処理をまとめる

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import { ToastProvider } from './components/Toast/Toast.jsx';
-import { AppRoutes } from './Routes.jsx' // Routes.jsxを参照
+import { Providers } from './Providers.jsx';
+import { AppRoutes } from './Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <ToastProvider>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </ToastProvider>
-    </AuthProvider>
+    <Providers>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </Providers>
   </StrictMode>
 )
